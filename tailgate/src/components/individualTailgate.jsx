446006266e9d25ec40f5604d522e2ee4d996007f
Yazan Awad <awadyazan25@gmail.com>
2021-12-02 21:15:12 -0800

    day 1 finishes

--- tailgate/src/components/individualTailgate.jsx
+++ tailgate/src/components/individualTailgate.jsx
@@ -1,15 +1,14 @@
+import "./componentStyle/individualTailgate.css";
+
 export default function IndividualTailgate(props) {
-  console.log("props");
-  console.log(props);
   const { tailgateName, time, location, spots, message } = props.tailgateInfo;
   return (
-    <div>
-      <h3>{tailgateName}</h3>
+    <div className="wrapper">
+      <h1>{tailgateName}</h1>
       <h3>
         {location}, {time}
       </h3>
       <h3>{spots} spots available!</h3>
-
       <h3>{message}</h3>
     </div>
   );
--- tailgate/src/pages/LoggedIn/ViewTailgates.jsx
+++ tailgate/src/pages/LoggedIn/ViewTailgates.jsx
@@ -1,44 +1,92 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import IndividualTailgate from "../../components/individualTailgate";
-
+import { Input } from "@chakra-ui/react";
 export default function ViewTailgates(props) {
   const { setLogin } = props;
-  const [tailgates, setTailgates] = useState([
+  const [search, setSearch] = useState([
     {
       tailgateName: "CASA",
       time: "1pm-3pm",
       location: "Mccarthy Quad",
       spots: 3,
-      message: "Fun tailgate",
+      message:
+        "Fun tailgate fsakfjhsalkfjklahklsafdkslahjsklafhsldkflsdsfkjsjlksjlk",
     },
   ]);
+  const tailgates = useRef();
   useEffect(() => {
-    setTailgates([
+    tailgates.current = [
       {
         tailgateName: "CASA",
         time: "1pm-3pm",
         location: "Mccarthy Quad",
         spots: 3,
-        message: "Fun tailgate",
+        message:
+          "Fun tailgate fsakfjhsalkfjklahklsafdkslahjsklafhsldkflsdsfkjsjlksjlk",
       },
-    ]);
+      {
+        tailgateName: "The boys",
+        time: "1pm-3pm",
+        location: "Mccarthy Quad",
+        spots: 3,
+        message:
+          "Fun tailgate fsakfjhsalkfjklahklsafdkslahjsklafhsldkflsdsfkjsjlksjlk",
+      },
+      {
+        tailgateName: "SEP",
+        time: "1pm-3pm",
+        location: "Mccarthy Quad",
+        spots: 3,
+        message:
+          "Fun tailgate fsakfjhsalkfjklahklsafdkslahjsklafhsldkflsdsfkjsjlksjlk",
+      },
+    ];
+    console.log(tailgates.current);
+    setSearch(tailgates.current);
   }, []);
+
   const signOut = () => {
     setLogin(false);
   };
+
   const renderTailgates = () => {
-    return (
-      <div>
-        {tailgates.map((tailgate) => (
-          <IndividualTailgate tailgateInfo={tailgate} />
-        ))}
-      </div>
-    );
+    if (search.length) {
+      return (
+        <div>
+          {search.map((tailgate) => (
+            <IndividualTailgate
+              key={tailgate.tailgateName}
+              tailgateInfo={tailgate}
+            />
+          ))}
+        </div>
+      );
+    } else {
+      return <h1>No Tailgates Found</h1>;
+    }
+  };
+
+  const searchVals = (value) => {
+    if (!value) {
+      setSearch(tailgates.current);
+    } else {
+      const newTailgates = tailgates.current.filter((val) => {
+        if (val.tailgateName.toLowerCase().includes(value.toLowerCase()))
+          return val;
+      });
+      setSearch(newTailgates);
+    }
   };
 
   return (
-    <div className="contain">
+    <div className="contain" style={{ margin: "auto" }}>
       <h1>View Tailgates</h1>
+      <Input
+        onChange={(e) => searchVals(e.target.value || null)}
+        placeholder="Basic usage"
+        size="md"
+        style={{ width: "400px", margin: "20px" }}
+      />
       {renderTailgates()}
       <input type="button" value="Sign Out" onClick={signOut} />
     </div>
